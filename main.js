de8d72b74a4b726cff9f74202a2ceeb0005dceea
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-03-12 16:44:49 +0100

    update_finish_base

---
+++ main.js
@@ -0,0 +1,26 @@
+
+$(function() {
+
+  $(".prev").click(
+    function () {
+     var imgActive = $('.images img.active');
+     imgActive.removeClass("active");
+     if (imgActive.hasClass("first") == true) {
+       $('.images img.last').addClass("active");
+     } else {
+       imgActive.prev().addClass("active");
+     }
+    }
+  );
+  $(".next").click(
+    function () {
+     var imgActive = $('.images img.active');
+     imgActive.removeClass("active");
+     if (imgActive.hasClass("last") == true) {
+       $('.images img.first').addClass("active");
+     } else {
+       imgActive.next().addClass("active");
+     }
+    }
+  );
+});
